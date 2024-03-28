a281a8b15ae7939841ea3cb142ed1a2e7f3a4416
o0grip0o <ossie.bodero@outlook.com>
2024-03-28 00:07:39 -0400

    Initial project commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+
+from crewai import Agent, Task, Crew
+from langchain_openai import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = "NA"
+
+llm = ChatOpenAI(
+    model = "crewai-llama2",
+    base_url = "http://localhost:11434/v1")
+
+general_agent = Agent(role = "Math Professor",
+                      goal = """Provide the solution to the students that are asking mathematical questions and give them the answer.""",
+                      backstory = """You are an excellent math professor that likes to solve math questions in a way that everyone can understand your solution""",
+                      allow_delegation = False,
+                      verbose = True,
+                      llm = llm)
+task = Task (description="""what is 3 + 5""",
+             agent = general_agent,
+             expected_output = "8")
+
+crew = Crew(
+            agents=[general_agent],
+            tasks=[task],
+            verbose=2
+        )
+
+result = crew.kickoff()
+
+print(result)
